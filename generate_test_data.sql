afae6b0bfbec1607fa691b63f5d68cb7fe420efc
Hiroaki Yutani <yutani@mierune.co.jp>
2025-06-09 14:00:09 +0900

    Test data

---
+++ generate_test_data.sql
@@ -0,0 +1,10 @@
+LOAD spatial;
+
+CREATE TABLE t_2048 AS
+    SELECT point, random() as val
+    FROM ST_GeneratePoints({min_x: 0, min_y:0, max_x:10, max_y:10}::BOX_2D, 2048, 42);
+CREATE TABLE t_2049 AS
+    SELECT point, random() as val
+    FROM ST_GeneratePoints({min_x: 0, min_y:0, max_x:10, max_y:10}::BOX_2D, 2049, 42);
+COPY t_2048 TO 'test/data/many_rows/points_2048.geojson' WITH (FORMAT GDAL, DRIVER 'GeoJSON');
+COPY t_2049 TO 'test/data/many_rows/points_2049.geojson' WITH (FORMAT GDAL, DRIVER 'GeoJSON');
