b86085e41b32f21be13a5ecaf8d0e50365ea44a5
Hiroaki Yutani <yutani.ini@gmail.com>
2026-04-02 21:34:41 +0900

    Support DATE and DATETIME (#49)
    * Update rusqlite
    
    * Support DATE and DATETIME
    
    * Bump version

--- generate_test_data.sql
+++ generate_test_data.sql
@@ -14,3 +14,22 @@ COPY t_2048 TO 'test/data/many_rows/points_2048.shp' WITH (FORMAT GDAL, DRIVER '
 COPY t_2049 TO 'test/data/many_rows/points_2049.geojson' WITH (FORMAT GDAL, DRIVER 'GeoJSON');
 COPY t_2049 TO 'test/data/many_rows/points_2049.gpkg' WITH (FORMAT GDAL, DRIVER 'GPKG');
 COPY t_2049 TO 'test/data/many_rows/points_2049.shp' WITH (FORMAT GDAL, DRIVER 'ESRI Shapefile');
+
+CREATE TABLE t_dates AS
+    SELECT
+        ST_Point(0, 0) as geom,
+        'event1' as name,
+        '2024-01-15'::DATE as event_date,
+        '2024-01-15 10:30:00'::TIMESTAMP as event_datetime
+    UNION ALL SELECT
+        ST_Point(1, 1),
+        'event2',
+        '2024-06-30'::DATE,
+        '2024-06-30 23:59:59.999'::TIMESTAMP
+    UNION ALL SELECT
+        ST_Point(2, 2),
+        'event3',
+        NULL,
+        NULL;
+
+COPY t_dates TO 'test/data/dates.gpkg' WITH (FORMAT GDAL, DRIVER 'GPKG');
